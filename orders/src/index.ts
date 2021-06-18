d96db7c2a7f5b07ff517520eb7329912650f2079
hamid tehrani <hamid.tehrani2003@gmail.com>
2021-06-18 11:40:51 +0430

    update start message

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -7,7 +7,7 @@ import { ExpirationComplateListener } from './events/listeners/expirtation-compl
 import { PaymentCreatedListener } from './events/listeners/payment-created-listener'
 
 const start = async () => {
-  console.log('Start...')
+  console.log('Start....')
 
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined')
